2294d5ca7dd9b6bb272bc7435c43ecb23fb3efff
YBardapurkar <ybardapurkar@gmail.com>
2021-04-01 18:32:21 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+
+class App extends Component {
+
+  constructor() {
+    super();
+		this.state = {
+        projects: [
+          {
+            id: 0,
+            title: "",
+            description: [],
+            links: [
+              {
+                id: 0,
+                type: "",
+                label: "",
+                url: ""
+              },
+            ],
+            tags: []
+          },
+        ]
+        
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        {
+          JSON.stringify(this.state.projects)
+        }
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    fetch("https://ybardapurkar-data.herokuapp.com/api/projects") // data source is an object, not an array.
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            projects: result,
+          });
+        },
+        error => {
+          this.setState({
+            error
+          });
+        }
+      );
+  }
 }
 
 export default App;
